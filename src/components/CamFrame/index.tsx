d6537f80a9c09b197fdd87ebb468b71d11673634
Guilherme Paes <guipaex@gmail.com>
2025-05-22 00:37:03 -0300

    :sparkles: implement React Vite

---
+++ src/components/CamFrame/index.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export default function CamFrame() {
+  return (
+    <div className="container">
+      <h1 id="camFrame">Camera</h1>
+      <div id="feed">@guipaex</div>
+    </div>
+  );
+}
